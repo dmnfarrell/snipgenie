993920cff83aff1ad432d8a3b1f01bd1a17c25f5
dmnfarrell <farrell.damien@gmail.com>
2020-10-05 11:02:38 +0100

    main script

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+    snpgenie GUI app lauancher.
+    Created Sep 2020
+    Copyright (C) Damien Farrell
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 3
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+from PySide2.QtWidgets import *
+from snpgenie import gui
+
+def main():
+    "Run the application"
+
+    import sys, os
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='pygenefinder tool')
+    parser.add_argument("-f", "--fasta", dest="filename",
+                        help="input fasta file", metavar="FILE")
+    args = vars(parser.parse_args())
+
+    app = QApplication(sys.argv)
+    aw = gui.App()
+    aw.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
